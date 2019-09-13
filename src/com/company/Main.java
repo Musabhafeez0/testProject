b6d2cf94c6a7ce96fe515c22955906dae5953bc2
Musabhafeez0 <54897275+Musabhafeez0@users.noreply.github.com>
2019-09-13 18:39:03 +0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("BOOM BOOM CHAO CHAO");
+        System.out.println("changed");
 	// write your code here
     }
 }
